98421ec909b5bbfb2a44ca91952e9be61fcbaf27
21-manic-016g <21-manic-016g@21-manic-016>
2024-04-22 17:31:15 +0900

    ccc

---
+++ oracletest/src/Desk/BoardExample1.java
@@ -0,0 +1,29 @@
+package Desk;
+
+public class BoardExample1 {
+	
+	public void list() {
+		System.out.println();
+		System.out.println("[게시물 목록]");
+		System.out.println("-------------------------------------------------------");
+		System.out.printf("%-6s%-12s%-16s%-40s\n", "no", "writer", "date", "title");
+		System.out.println("-------------------------------------------------------");
+		System.out.printf("%-6s%-12s%-16s%-40s\n", "1", "winer", "2022.01.27", "게시판에 오신 것을 환영합니다.");
+		System.out.printf("%-6s%-12s%-16s%-40s\n", "2", "winter", "2022.01.27", "올 겨울은 많이 춥습니다.");
+		mainMenu();
+	}
+	
+	public void mainMenu() {
+		System.out.println();
+		System.out.println("-------------------------------------------------------");
+		System.out.println("메인 메뉴: 1.Create | 2.Read | 3.Clear | 4.Exit");
+		System.out.printf("메뉴 선택: ");
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BoardExample1 boardExample = new BoardExample1();
+		boardExample.list();
+	}
+}
